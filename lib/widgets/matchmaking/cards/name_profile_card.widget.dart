1bc429f7849b9df7f37e3d9e19dd9531a523e185
Tony Heng <tony.heng@epita.fr>
2022-04-03 09:45:44 +0200

    wip: name profil card

---
+++ lib/widgets/matchmaking/cards/name_profile_card.widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:trip_n_joy_front/app_localizations.dart';
+import 'package:trip_n_joy_front/providers/matchmaking/matchmaking.provider.dart';
+import 'package:trip_n_joy_front/widgets/common/card.widget.dart';
+import 'package:trip_n_joy_front/widgets/common/input.widget.dart';
+
+import '../../common/button.widget.dart';
+import '../../common/checkbox.widget.dart';
+
+class NameProfileCard extends HookConsumerWidget {
+  const NameProfileCard({Key? key}) : super(key: key);
+
+  final name = "name_profile";
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final profileName = useState("");
+    final matchmakingService = ref.watch(matchmakingProvider.notifier);
+    return StandardCard(
+      name: "NameProfileCard",
+      title: AppLocalizations.of(context).translate("card.name_profile.title"),
+      subtitle: AppLocalizations.of(context).translate("card.name_profile.title"),
+      shadowColor: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
+      child: Column(
+        children: [
+          Expanded(
+            child: InputField(
+              label: AppLocalizations.of(context).translate("card.name_profile.label"),
+              onChanged: (value) => profileName.value = value,
+              hint: AppLocalizations.of(context).translate("card.name_profile.hint"),
+            ),
+          ),
+          PrimaryButton(
+            text: AppLocalizations.of(context).translate('common.validate'),
+            onPressed: () {
+              matchmakingService.submitCard(name, profileName.value);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
