8e572f1fecb8f92bca3f3c8d88f77cd82869f319
Tony Heng <tony.heng@epita.fr>
2022-04-04 16:17:54 +0200

    feat: translation for profile creation card

--- lib/widgets/matchmaking/cards/profile_creation.widget.dart
+++ lib/widgets/matchmaking/cards/profile_creation.widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:trip_n_joy_front/app_localizations.dart';
 import 'package:trip_n_joy_front/providers/matchmaking/matchmaking.provider.dart';
 
 import '../../common/button.widget.dart';
@@ -14,13 +15,15 @@ class ProfileCreation extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final matchmakingService = ref.watch(matchmakingProvider.notifier);
     return StandardCard(
-      title: "Création du profil voyage",
-      subtitle: "Cliquer pour démarrer la création du profil voyage",
+      title: AppLocalizations.of(context).translate('cards.creation_profile.title'),
+      subtitle: AppLocalizations.of(context).translate('cards.creation_profile.subtitle'),
       color: Theme.of(context).colorScheme.primary,
       backgroundColor: Theme.of(context).colorScheme.onPrimary,
       shadowColor: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
       child: Center(
-        child: PrimaryButton(text: "Démarrer", onPressed: () => matchmakingService.startProfileCreation()),
+        child: PrimaryButton(
+            text: AppLocalizations.of(context).translate('common.start'),
+            onPressed: () => matchmakingService.startProfileCreation()),
       ),
     );
   }
