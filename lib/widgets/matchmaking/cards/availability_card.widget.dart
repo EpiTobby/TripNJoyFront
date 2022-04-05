2d514a3985fcea1867bf650acf9502d64d41ccda
Tony Heng <tony.heng@epita.fr>
2022-04-05 14:54:14 +0200

    wip: availability card

--- lib/widgets/matchmaking/cards/availability_card.widget.dart
+++ lib/widgets/matchmaking/cards/availability_card.widget.dart
@@ -8,20 +8,19 @@ import 'package:trip_n_joy_front/widgets/common/input.widget.dart';
 
 import '../../common/button.widget.dart';
 
-class NameProfileCard extends HookConsumerWidget {
-  const NameProfileCard({Key? key, this.isLoading = false}) : super(key: key);
+class AvailabilityCard extends HookConsumerWidget {
+  const AvailabilityCard({Key? key, this.isLoading = false}) : super(key: key);
 
   final bool isLoading;
-  final name = "name_profile";
+  final name = "availability";
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final profileName = useState("");
     final matchmakingService = ref.watch(matchmakingProvider.notifier);
     return StandardCard(
       name: "NameProfileCard",
-      title: AppLocalizations.of(context).translate("cards.name_profile.title"),
-      subtitle: AppLocalizations.of(context).translate("cards.name_profile.subtitle"),
+      title: AppLocalizations.of(context).translate("cards.availability.title"),
+      subtitle: AppLocalizations.of(context).translate("cards.availability.subtitle"),
       shadowColor: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
       isLoading: isLoading,
       child: Column(
@@ -31,19 +30,19 @@ class NameProfileCard extends HookConsumerWidget {
               padding: const EdgeInsets.symmetric(horizontal: 30.0),
               child: Center(
                 child: InputField(
-                  label: AppLocalizations.of(context).translate("cards.name_profile.label"),
-                  onChanged: (value) => profileName.value = value,
+                  label: AppLocalizations.of(context).translate("cards.availability.begin"),
+                  onChanged: (value) {},
                 ),
               ),
             ),
           ),
-          PrimaryButton(
-            text: AppLocalizations.of(context).translate('common.validate'),
-            isDisabled: profileName.value.isEmpty,
-            onPressed: () {
-              matchmakingService.submitCard(name, profileName.value);
-            },
-          ),
+          // PrimaryButton(
+          //   text: AppLocalizations.of(context).translate('common.validate'),
+          //   isDisabled: .value.isEmpty,
+          //   onPressed: () {
+          //     matchmakingService.submitCard(name, profileName.value);
+          //   },
+          // ),
         ],
       ),
     );
