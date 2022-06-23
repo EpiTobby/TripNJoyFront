8e327087a4fe668225e5431f9bee04d244ccce71
Tony Heng <tony.heng@epita.fr>
2022-06-23 20:02:15 +0200

    feat: add flutter_osm_plugin

--- lib/screens/groups/place_suggestion.screen.dart
+++ lib/screens/groups/place_suggestion.screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_map/flutter_map.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:trip_n_joy_front/app_localizations.dart';
@@ -12,6 +11,7 @@ import 'package:trip_n_joy_front/services/log/logger.service.dart';
 import 'package:trip_n_joy_front/widgets/common/async_value.widget.dart';
 import 'package:trip_n_joy_front/widgets/common/layout_box.widget.dart';
 import 'package:trip_n_joy_front/widgets/groups/planning_activity.widget.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
 class PlaceSuggestion extends HookConsumerWidget {
   const PlaceSuggestion({
@@ -29,8 +29,9 @@ class PlaceSuggestion extends HookConsumerWidget {
       Future.microtask(() => ref.read(planningProvider).clearSuggestedActivities());
     }, [place]);
     final activities = ref.watch(planningProvider).suggestedActivities;
-    final currentPosition = useState(LatLng(48.864716, 2.349014));
+    // final currentPosition = useState(LatLng(48.864716, 2.349014));
     final isLoading = useState(false);
+    final controller = useState(MapController());
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context).translate("groups.planning.activity.type.$place"),
@@ -41,7 +42,7 @@ class PlaceSuggestion extends HookConsumerWidget {
       ),
       body: isLoading.value
           ? const Center(child: CircularProgressIndicator())
-          : ListView(
+          : Column(
               children: [
                 Padding(
                   padding: const EdgeInsets.all(16.0),
@@ -56,94 +57,121 @@ class PlaceSuggestion extends HookConsumerWidget {
                   child: SizedBox(
                     width: double.infinity,
                     height: 200,
-                    child: FlutterMap(
-                      options: MapOptions(
-                        interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate,
-                        center: currentPosition.value,
-                        zoom: 13.0,
-                        onPositionChanged: (position, hasGesture) {
-                          if (position.center != currentPosition.value) {
-                            currentPosition.value = position.center!;
-                          }
-                        },
-                        onLongPress: (position, hasGesture) {
-                          ref.read(planningProvider).getSuggestedActivities(place, currentPosition.value);
-                        },
-                      ),
+                    child: Stack(
                       children: [
-                        TileLayerWidget(
-                          options: TileLayerOptions(
-                            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                            subdomains: ['a', 'b', 'c'],
+                        OSMFlutter(
+                          controller: controller.value,
+                          trackMyPosition: true,
+                          showZoomController: true,
+                          minZoomLevel: 5,
+                          maxZoomLevel: 18,
+                          stepZoom: 1.0,
+                          isPicker: true,
+                          userLocationMarker: UserLocationMaker(
+                            personMarker: MarkerIcon(
+                              icon: Icon(
+                                Icons.location_on,
+                                color: Theme.of(context).colorScheme.secondary,
+                                size: 64,
+                              ),
+                            ),
+                            directionArrowMarker: MarkerIcon(
+                              icon: Icon(
+                                Icons.double_arrow,
+                                size: 48,
+                              ),
+                            ),
+                          ),
+                          markerOption: MarkerOption(
+                            defaultMarker: MarkerIcon(
+                              icon: Icon(
+                                Icons.location_on,
+                                color: Theme.of(context).colorScheme.secondary,
+                                size: 64,
+                              ),
+                            ),
                           ),
                         ),
                         Positioned(
-                          bottom: 8,
-                          right: 8,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              ref.read(planningProvider).getSuggestedActivities(place, currentPosition.value);
-                            },
-                            style: ElevatedButton.styleFrom(
-                              primary: Theme.of(context).colorScheme.secondary,
-                              textStyle: TextStyle(fontSize: 16, color: Theme.of(context).colorScheme.onSecondary),
-                              padding: const EdgeInsets.all(10),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50),
-                              ),
-                              minimumSize: const Size(0, 0),
+                          bottom: 10,
+                          left: 10,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Theme.of(context).colorScheme.secondary,
                             ),
-                            child: Icon(Icons.search, color: Theme.of(context).colorScheme.onSecondary),
+                            child: IconButton(
+                              padding: const EdgeInsets.all(8),
+                              onPressed: () async {
+                                await controller.value.currentLocation();
+                                await controller.value.setZoom(zoomLevel: 14);
+                              },
+                              icon: Icon(Icons.location_searching, color: Theme.of(context).colorScheme.onSecondary),
+                            ),
                           ),
                         ),
-                      ],
-                      layers: [
-                        MarkerLayerOptions(
-                          markers: [
-                            Marker(
-                              width: 80.0,
-                              height: 80.0,
-                              point: currentPosition.value,
-                              builder: (ctx) => Icon(
-                                Icons.location_on,
-                                color: Theme.of(context).colorScheme.secondary,
-                              ),
+                        Positioned(
+                          bottom: 10,
+                          right: 10,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Theme.of(context).colorScheme.secondary,
                             ),
-                          ],
+                            child: IconButton(
+                              padding: const EdgeInsets.all(8),
+                              onPressed: () async {
+                                final position = await controller.value.centerMap;
+                                ref.read(planningProvider).getSuggestedActivities(place, position);
+                              },
+                              icon: Icon(Icons.search, color: Theme.of(context).colorScheme.onSecondary),
+                            ),
+                          ),
                         ),
+                        Positioned.fill(
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Icon(Icons.location_on, color: Theme.of(context).colorScheme.secondary),
+                          ),
+                        )
                       ],
                     ),
                   ),
                 ),
-                LayoutBox(
-                  title: AppLocalizations.of(context).translate("groups.planning.activity.suggestion.title"),
-                  children: <Widget>[
-                    AsyncValueWidget<List<PlaceResponse>>(
-                      value: activities,
-                      data: (activities) => Column(
-                        children: activities
-                            .map(
-                              (activity) => PlanningActivity(
-                                title: activity.name,
-                                subtitle: activity.street,
-                                subsubtitle: activity.city,
-                                description: activity.country,
-                                color: ActivityColors.getRandomColorFromString(activity.name),
-                                onTap: () async {
-                                  isLoading.value = true;
-                                  final newActivity =
-                                      ref.read(planningProvider).getSuggestedActivity(groupId, place, activity);
-                                  isLoading.value = false;
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: LayoutBox(
+                      title: AppLocalizations.of(context).translate("groups.planning.activity.suggestion.title"),
+                      children: <Widget>[
+                        AsyncValueWidget<List<PlaceResponse>>(
+                          value: activities,
+                          data: (activities) => Column(
+                            children: activities
+                                .map(
+                                  (activity) => PlanningActivity(
+                                    title: activity.name,
+                                    subtitle: activity.street,
+                                    subsubtitle: activity.city,
+                                    description: activity.country,
+                                    color: ActivityColors.getRandomColorFromString(activity.name),
+                                    onTap: () async {
+                                      isLoading.value = true;
+                                      final newActivity =
+                                          ref.read(planningProvider).getSuggestedActivity(groupId, place, activity);
+                                      isLoading.value = false;
 
-                                  Navigator.of(context).push(MaterialPageRoute(
-                                      builder: (_) => EditActivity(groupId: groupId, suggestedActivity: newActivity)));
-                                },
-                              ),
-                            )
-                            .toList(),
-                      ),
+                                      Navigator.of(context).push(MaterialPageRoute(
+                                          builder: (_) =>
+                                              EditActivity(groupId: groupId, suggestedActivity: newActivity)));
+                                    },
+                                  ),
+                                )
+                                .toList(),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ],
             ),
