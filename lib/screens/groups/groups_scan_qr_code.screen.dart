e78f5d44243d537b66f57b961a1a5ae99b3c08d9
Tony Heng <tony.heng@epita.fr>
2022-07-17 23:39:47 +0200

    feat: add scan square

--- lib/screens/groups/groups_scan_qr_code.screen.dart
+++ lib/screens/groups/groups_scan_qr_code.screen.dart
@@ -24,31 +24,51 @@ class GroupsScanQRCode extends HookConsumerWidget {
         backgroundColor: Theme.of(context).colorScheme.onPrimary,
         shadowColor: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
       ),
-      body: MobileScanner(
-        allowDuplicates: false,
-        controller: MobileScannerController(facing: CameraFacing.back),
-        onDetect: (barcode, args) {
-          if (isHandlingQRCode.value) {
-            return;
-          }
-          isHandlingQRCode.value = true;
-          if (barcode.rawValue == null) {
-            debugPrint('Failed to scan Barcode');
-            isHandlingQRCode.value = false;
-          } else {
-            final String code = barcode.rawValue!;
-            qrCodeViewModel.extractGroupIdFromQRCode(code);
-            debugPrint('Barcode found! $code');
-            showBarModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return const GroupInvitationDialog();
-              },
-            ).whenComplete(() {
-              isHandlingQRCode.value = false;
-            });
-          }
-        },
+      body: Stack(
+        children: [
+          MobileScanner(
+            allowDuplicates: false,
+            controller: MobileScannerController(facing: CameraFacing.back),
+            onDetect: (barcode, args) {
+              if (isHandlingQRCode.value) {
+                return;
+              }
+              isHandlingQRCode.value = true;
+              if (barcode.rawValue == null) {
+                debugPrint('Failed to scan Barcode');
+                isHandlingQRCode.value = false;
+              } else {
+                final String code = barcode.rawValue!;
+                qrCodeViewModel.extractGroupIdFromQRCode(code);
+                debugPrint('Barcode found! $code');
+                showBarModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return const GroupInvitationDialog();
+                  },
+                ).whenComplete(() {
+                  isHandlingQRCode.value = false;
+                });
+              }
+            },
+          ),
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.width * 0.8,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: Theme.of(context).colorScheme.secondary,
+                    width: 4,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
