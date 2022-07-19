335b46db1245dfc7f40b84ef145a23692579a304
Tony Heng <tony.heng@epita.fr>
2022-07-19 12:09:10 +0200

    feat: add generate QRCode button in group settings

---
+++ lib/widgets/common/dialog/qr_code_dialog.widget.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:trip_n_joy_front/app_localizations.dart';
+import 'package:trip_n_joy_front/providers/groups/qr_code.provider.dart';
+import 'package:trip_n_joy_front/widgets/common/layout/layout_item.widget.dart';
+import 'package:trip_n_joy_front/widgets/common/layout/layout_item_value.widget.dart';
+
+class QRCodeDialog extends HookConsumerWidget {
+  const QRCodeDialog({
+    Key? key,
+    required this.groupId,
+  }) : super(key: key);
+
+  final int groupId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final qrCodeViewModel = ref.watch(qrCodeProvider.notifier);
+    return AnimatedPadding(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+      ),
+      curve: Curves.easeOut,
+      duration: const Duration(milliseconds: 200),
+      child: Material(
+        child: SafeArea(
+          top: false,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Ink.image(
+                    fit: BoxFit.cover,
+                    width: 200,
+                    height: 200,
+                    image: const NetworkImage(
+                        "https://upload.wikimedia.org/wikipedia/commons/7/78/Qrcode_wikipedia_fr_v2clean.png"),
+                  ),
+                ),
+                LayoutItem(
+                  child: LayoutItemValue(
+                    value: AppLocalizations.of(context).translate("groups.qr_code.download"),
+                    icon: Icons.download,
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
