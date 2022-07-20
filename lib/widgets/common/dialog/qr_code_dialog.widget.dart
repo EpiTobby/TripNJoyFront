10057e0c0001c639bd6e58a63d650eaa21d2aa05
Tony Heng <tony.heng@epita.fr>
2022-07-20 14:11:12 +0200

    feat: add share_extend dep wip: share qrcode

--- lib/widgets/common/dialog/qr_code_dialog.widget.dart
+++ lib/widgets/common/dialog/qr_code_dialog.widget.dart
@@ -4,6 +4,7 @@ import 'package:trip_n_joy_front/app_localizations.dart';
 import 'package:trip_n_joy_front/providers/groups/qr_code.provider.dart';
 import 'package:trip_n_joy_front/widgets/common/layout/layout_item.widget.dart';
 import 'package:trip_n_joy_front/widgets/common/layout/layout_item_value.widget.dart';
+import 'package:share_extend/share_extend.dart';
 
 class QRCodeDialog extends HookConsumerWidget {
   const QRCodeDialog({
@@ -15,7 +16,9 @@ class QRCodeDialog extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: get QrCode from provider
     final qrCodeViewModel = ref.watch(qrCodeProvider.notifier);
+    const qrCodeUrl = "https://upload.wikimedia.org/wikipedia/commons/7/78/Qrcode_wikipedia_fr_v2clean.png";
     return AnimatedPadding(
       padding: EdgeInsets.only(
         bottom: MediaQuery.of(context).viewInsets.bottom,
@@ -37,15 +40,17 @@ class QRCodeDialog extends HookConsumerWidget {
                     fit: BoxFit.cover,
                     width: 200,
                     height: 200,
-                    image: const NetworkImage(
-                        "https://upload.wikimedia.org/wikipedia/commons/7/78/Qrcode_wikipedia_fr_v2clean.png"),
+                    image: const NetworkImage(qrCodeUrl),
                   ),
                 ),
                 LayoutItem(
                   child: LayoutItemValue(
-                    value: AppLocalizations.of(context).translate("groups.qr_code.download"),
-                    icon: Icons.download,
-                    onPressed: () {},
+                    value: AppLocalizations.of(context).translate("groups.qr_code.share"),
+                    icon: Icons.share,
+                    onPressed: () {
+                      // TODO: share qr code
+                      ShareExtend.share(qrCodeUrl, "text");
+                    },
                   ),
                 ),
               ],
